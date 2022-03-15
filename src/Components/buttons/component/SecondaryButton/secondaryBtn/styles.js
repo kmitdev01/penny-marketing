34e0f47fc334e0fee83d9f80c4b3b18aa3b6c1f7
harpreet780 <harpreetsaini276@gmail.com>
2022-03-15 11:14:18 +0530

    remove theme

--- src/Components/buttons/component/SecondaryButton/secondaryBtn/styles.js
+++ src/Components/buttons/component/SecondaryButton/secondaryBtn/styles.js
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
-import { space ,fontSizes ,colors } from '../../../../../constants/theme';
 export const Button = styled.div`
     display:flex;
     align-items:center;
-    background: ${colors.secondary};
+    background:  #FDB016;
     width:100%;
-    padding: ${space[0] + 1}px ${space[10] + 4}px;
-    color: ${colors.white};
+    padding: 1px 24px;
+    color: #fff;
     font-weight: 700;
-    font-size:${fontSizes[2] + 2}px;
+    font-size: 22px;
     line-height: 26px;
     height:48px;
     border-radius:84.5px;
--- src/Pages/Pennyprofit/style.js
+++ src/Pages/Pennyprofit/style.js
@@ -1,54 +1,67 @@
 import styled from 'styled-components';
-import { space ,fontSizes ,colors ,fullWidth } from '../../constants/theme';
 export const Wrapper = styled.div`
-   background-image: ${colors.background};
+   background-image: linear-gradient(180deg, #FFFFFF 0%, #E1E5E9 100%);
 `
 export const InnerWrap = styled.div`
-  padding:${space[10] * 2}px 0px;
+  padding:40px 0px;
 `
 export const Section = styled.div`
    display:flex;
    align-items:center;
+   @media only screen and (max-width:1024px){
+       flex-wrap: wrap;
+   }
 `
 export const ContentWrap = styled.div`
-    width: ${fullWidth};
+    width: 100%;
 `
 export const Heading = styled.h6`
     font-family: 'Gotham Rounded';
     font-style: normal;
     font-weight: 700;
-    font-size: ${fontSizes[5] + 8}px;
+    font-size: 56px;
     line-height: 68px;
     text-transform: uppercase;
-    color: ${colors.text};
+    color: #454545;
     text-align:left;
+    @media only screen and (max-width:1024px){
+      br{
+         display:none;
+      }
+   }
 `
 export const Border = styled.div`
-   border-bottom 2.4px solid ${colors.primary};
+   border-bottom 2.4px solid #04A454;
    width: 417px;
+   @media only screen and (max-width:1024px){
+      width: 100%;
+   }
 `
 export const BottomWrap = styled.div`
    display:flex;
    align-items:center;
-   padding:${space[10] + 47}px 0px;
+   padding:67px 0px;
    img{
-       margin-right: ${space[6]}px;
+       margin-right: 12px;
+   }
+   @media only screen and (max-width:1024px){
+      flex-wrap: wrap;
    }
 `
 export const Text = styled.p`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 300;
-    font-size: ${fontSizes[4] + 1}px;
+    font-size: 33px;
     line-height: 39px;
-    color: ${colors.text};
+    color: #454545;
     text-align:left;
-    margin-top: ${space[10]  + 1}px;
-    margin-bottom: ${space[10]  + 4}px;
+    margin-top: 21px;
+    margin-bottom: 24px;
      &.title{
         font-weight: 700; 
         margin-top:0;
-        margin-bottom: ${space[7]}px;
+        margin-bottom:14px;
      }
      &.subtitle{
         margin-top:0;
@@ -56,30 +69,33 @@ export const Text = styled.p`
      }
 `
 export const Imgbox = styled.div`
-     width: ${fullWidth};
+     width: 100%;
 //    img{ 
-//       max-width: ${fullWidth};
+//       max-width: 100%;
 //    } 
 `
 export const ImgWrap = styled.div`
+   @media only screen and (max-width:1024px){
+      width: 100%;
+   }
 `
 export const SubHeading = styled.p`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 300;
-    font-size: ${fontSizes[4] + 1}px;
+    font-size: 33px;
     line-height: 39px;
-    color: ${colors.text};
+    color: #454545;
     text-align:left;
-    margin-bottom: ${space[10]}px;
+    margin-bottom:20px;
 `
 export const SavingWrap= styled.div`
-   width: ${fullWidth};
-   background: ${colors.white};
+   width: 100%;
+   background: #fff;
 `
 export const Wrap= styled.div`
-   width: ${fullWidth};
-   padding:${space[10] * 3}px;
+   width: 100%;
+   padding:30px 0px;
    display: flex;
    align-items:center;
    justify-content:center;
